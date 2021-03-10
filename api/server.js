c176cf592533380757af1002d2b3e307a4ed00ec
ChadDiaz <chadcdiaz@gmail.com>
2021-03-09 21:33:23 -0500

    first 2 request are completed

--- api/server.js
+++ api/server.js
@@ -1,7 +1,34 @@
 // BUILD YOUR SERVER HERE
+const User = require("./users/model.js");
 const express = require("express");
 const server = express();
 server.use(express.json());
+
+// | GET    | /api/users     | Returns an array
+server.get("/api/users", async (req, res) => {
+  try {
+    const user = await User.find();
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// GET    | /api/users/:id | Returns the user object with the specified `id`.
+
+server.get("/api/users/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = await User.findById(id);
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// POST   | /api/users     | Creates a user using the information sent inside the `request body`.
+
 
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
