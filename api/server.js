ac949d30d48a1ca0f010d6515482a5c97be2d9f7
ORiveraJr84 <oriverajr84@gmail.com>
2021-03-14 23:35:22 -0400

    Server set up, get & post methods complete.

--- api/server.js
+++ api/server.js
@@ -1,3 +1,45 @@
 // BUILD YOUR SERVER HERE
+const express = require("express");
+const Users = require("./users/model");
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express();
+server.use(express.json());
+
+server.get("/api/users", async (req, res) => {
+  try {
+    const users = await Users.find();
+    res.json({ users });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+server.put("/api/users", async (req, res) => {
+  const newUser = req.body;
+
+  try {
+    await Users.insert(newUser);
+    res.json(newUser);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: err });
+  }
+});
+
+/* server.put("/api/dogs/:id", async (req, res) => {
+  const { id } = req.params;
+  const dog = req.body;
+  try {
+    const updatedDog = await Dog.update(id, dog);
+    if (updatedDog) {
+      res.json(updatedDog);
+    } else {
+      res.status(404).json({ messsage: "bad id" });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: err });
+  }
+}); */
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
