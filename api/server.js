d183d5719b3a2386ac4e3b17562998fb2f525db4
ORiveraJr84 <oriverajr84@gmail.com>
2021-05-07 01:10:15 -0400

    Root endpoint for testing set up.

--- api/server.js
+++ api/server.js
@@ -3,4 +3,12 @@ const express = require("express");
 
 const server = express();
 
+server.use(express.json());
+
+server.get("/", (req, res) => {
+  res
+    .status(200)
+    .send({ message: "The server is up and running successfully." });
+});
+
 module.exports = server; // EXPORT YOUR SERVER instead of {}
