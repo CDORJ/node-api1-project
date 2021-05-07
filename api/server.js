fc623c8bc92a77033cf4fc4e0a636efb40d48a19
ORiveraJr84 <oriverajr84@gmail.com>
2021-05-07 01:16:31 -0400

    Get all users complete.

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 // BUILD YOUR SERVER HERE
 const express = require("express");
+const users = require("./users/model");
 
 const server = express();
 
@@ -9,6 +10,15 @@ server.get("/", (req, res) => {
   res
     .status(200)
     .send({ message: "The server is up and running successfully." });
+});
+
+server.get("/api/users", async (req, res) => {
+  const listOfUsers = await users.find();
+  if (listOfUsers.length < 1) {
+    res.status(200).send({ message: "There are no users in the database." });
+  } else {
+    res.status(200).send(listOfUsers);
+  }
 });
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
